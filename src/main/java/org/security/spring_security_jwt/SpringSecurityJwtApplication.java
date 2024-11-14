de3fc352b8e034a49dc013ce4a881a74a030237d
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-11-14 18:43:57 +0600

    Spring Security jwt demo & final layer impl

--- src/main/java/org/security/spring_security_jwt/SpringSecurityJwtApplication.java
+++ src/main/java/org/security/spring_security_jwt/SpringSecurityJwtApplication.java
@@ -1,13 +1,48 @@
 package org.security.spring_security_jwt;
 
+import org.security.spring_security_jwt.auth.AuthenticationService;
+import org.security.spring_security_jwt.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.security.spring_security_jwt.user.Role.ADMIN;
+import static org.security.spring_security_jwt.user.Role.MANAGER;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SpringSecurityJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
 	}
 
 }
